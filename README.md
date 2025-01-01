be77abba697bc44529c0a4b9067ec216d8478eb0
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-01-01 12:47:37 +0100

    diverging parameters

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # Modelling Quantum Hardware: Project
 Final project for the course *Modeling quantum hardware: open dynamics and control* by *Javier del Pino* at the *University Konstanz*.
 
-## Project Description
-Researching squeezing in a quantum optomechanical system.
-steps:
-	- Implement a optomechanical cavity. 
+[notes.pdf](notes.pdf)
--- scripts/02 linerized time evolution.jl
+++ scripts/02 linerized time evolution.jl
@@ -9,19 +9,18 @@ cd(@__DIR__)
 g0 = 0.01*ω_m        # Single-photon coupling (Hz)
 Δ = ω_m             # Detuning (drive on red sideband)
 drive_amplitude = 100*ω_m  # Drive amplitude (Hz)
-n_th = 1000           # Thermal occupation number
+n_th = 0           # Thermal occupation number
 
 
 # Steady-state amplitudes (classical solution)
 α = drive_amplitude / (κ/2 - 1im * Δ)  # Steady-state optical field
-β = 0                                  # Mechanical mode assumed at rest
 
 # Effective coupling
 G = g0 * abs(α)  # Linearized coupling strength
 
 # Operators
 optical_space = FockBasis(10)
-mechanical_space = FockBasis(20)
+mechanical_space = FockBasis(10)
 hilbert_space = optical_space ⊗ mechanical_space
 
 δa = destroy(optical_space) ⊗ one(mechanical_space)
@@ -31,7 +30,8 @@ hilbert_space = optical_space ⊗ mechanical_space
 H_c = Δ * dagger(δa) * δa
 H_m = ω_m * dagger(δb) * δb
 H_int = -G * (δa + dagger(δa)) * (δb + dagger(δb))
-H = H_c + H_m + H_int
+H_pump = drive_amplitude * (δa + dagger(δa))
+H = H_c + H_m + H_int + H_pump
 
 # Collapse operators
 C_optical = √κ * δa
@@ -43,13 +43,12 @@ collapse_ops = [C_optical, C_mechanical, C_thermal]
 ψ0 = tensor(fockstate(optical_space, 0), fockstate(mechanical_space, 0))
 
 # Time evolution
-times = 0:0.05:3
+times = 0:0.01:3
 @time tout, result = timeevolution.master(times, ψ0, H, collapse_ops);
 
 # %%
 # look at timeevolution
-
-function getStates(state, index::Integer)
+function numberStates(state, index::Integer)
 	trace = ptrace.(state, index)
 	return hcat([diag(normalize(t).data) for t in trace]...)'	
 end
@@ -61,13 +60,13 @@ f = Figure(size=fullsize)
 a = Axis(f[1,1], xlabel="Time in 2π/ωₘ", ylabel="n")
 
 lines!(times, abs.(N_opt), label="Optical")
-heatmap!(times, 0:optical_space.N, abs.(getStates(result, 2)), 
+heatmap!(times, 0:optical_space.N, abs.(numberStates(result, 2)), 
  colormap=[:transparent, Makie.wong_colors()[1]]
 )
 
 lines!(times, abs.(N_mech), label="Mechanical")
 
-axislegend(position=:rb)
+axislegend(position=:rb, padding=0, framevisible=false)
 
 save("../figures/02 time evolution.pdf", f)
 f
