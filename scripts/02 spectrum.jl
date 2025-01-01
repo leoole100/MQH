29a3fb3da3e078d30b10730b643f817d8b251d9b
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-01-01 14:42:35 +0100

    simple spectrum

--- scripts/02 spectrum.jl
+++ scripts/02 spectrum.jl
@@ -46,16 +46,14 @@ function HJ(;
 	return H, J
 end
 
-times_full = 0:0.2:50
-@time tout, result_full = timeevolution.master(times_full, ψ0, HJ(
-	# γ_m=0
-)...);
+times_full = 0:0.25:50
+@time tout, result_full = timeevolution.master(times_full, ψ0, HJ()...);
 
 cutoff = 0
 result = result_full[times_full .>= cutoff]
 times = times_full[times_full .>= cutoff]
 S_xx = abs2.(fft(expect(x, result)))
-f = frequencies = fftfreq(length(times), times[2]-times[1])
+frequencies = fftfreq(length(times), 1/(times[2]-times[1]))
 S_xx = S_xx[frequencies .> 0]
 frequencies = frequencies[frequencies .> 0]
 
