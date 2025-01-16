29f35a2c561d7a9c8d27e236fc92c92588f3a42b
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-01-16 20:30:06 +0100

    added a small harmonic oscillator example

--- scripts/03 harmonic oscillator.jl
+++ scripts/03 harmonic oscillator.jl
@@ -1,24 +1,45 @@
-cd(@__DIR__)
-
-# make figure style
-using CairoMakie
-inch = 96
-pt = 4/3
-cm = inch/2.54
-update_theme!(
-	fontsize=10pt,
-	px_per_unit = 10,
-	fonts = (;
-		regular="Roboto Regular",
-		bold="Roboto Bold",
-	),
-)
-halfsize = (2inch, (2/1.33)inch)
-fullsize = (4inch, (4/1.33)inch)
-
-
-# Define the basis
+include("00 functions.jl")
 using QuantumOptics
 
-space = FockBasis(10)
 
+space = FockBasis(20)
+
+a = destroy(space)
+at = dagger(a)
+
+x = a + at
+p = im*(at-a)
+H() = 1/2*p^2 + 1/2*x^2
+ψ0 = coherentstate(space, 1)
+
+dt = 0.001
+η = .8
+O = x
+
+# C(η=1) = η*x 
+C(η=1) = η*x
+
+# time evolution
+times = 0:0.1:10
+
+tout, free = timeevolution.schroedinger(times, ψ0, H())
+@time measured = [stochastic.master(times, ψ0, H(), [], [C(η)], dt=dt) for i in 1:10];
+
+#%%
+
+f = Figure(size=fullsize)
+a = Axis(f[1,1], xlabel="t", ylabel="Re <x>", title="dt = $(dt), η = $(η)")
+lines!(times, real.(expect(O, free)), label="free", color=:black)
+
+for (t, m) in measured
+	if length(t)==length(times)
+		lines!(t, real.(expect(O, m)), color=:green, alpha=.25, label="measured")	
+	else
+		lines!(t, real.(expect(O, m)), color=:red, alpha=.25, label="instability detected")	
+	end
+end
+
+axislegend(padding=0, merge=true, position=:lb, framevisible=false)
+ylims!(-10, 10)
+save("../figures/03 harmonic oscillator.pdf", f)
+f
