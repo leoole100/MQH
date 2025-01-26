ed57d70a62b384c8109b06dc73189b5dc2f67c17
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-01-26 08:58:37 +0100

    made nicer interface for zeno effect

--- scripts/01 zeno.jl
+++ scripts/01 zeno.jl
@@ -11,8 +11,8 @@ Nc = 3
 κ₂ = 0.3κ
 g = 0.2κ
 β = 0.2*sqrt(κ)
-gₛ = 5*1e-2κ
-T = [0:0.1:3/gₛ;]
+# gₛ = 5*1e-2κ
+# T = [0:0.1:3/gₛ;]
 
 # Basis and operators
 bc = FockBasis(Nc)
@@ -24,22 +24,14 @@ ad = create(bc) ⊗ one(ba)
 σ = one(bc) ⊗ sigmam(ba)
 n = σ⁺*σ
 
-# %%
-
 # Hamiltonian
-H0 = g*ad*a*n
-Hp = gₛ*(σ⁺ + σ)
-
-# Coherent drive of cavity
-Hf = -1.0im*β*(ad - a)
-
-H = H0 + Hf + Hp
+H0(g) = g*ad*a*n
+Hp(gₛ) = gₛ*(σ⁺ + σ)
+Hf(β=β) = -1.0im*β*(ad - a) # Coherent drive of cavity
 
 # Damping operators of master equation
 J = [a]
 rates = [κ]
-
-# %%
 
 # Stochastic damping operators
 C = [sqrt(κ₂)*a]
@@ -48,35 +40,41 @@ C = [sqrt(κ₂)*a]
 ψ0 = fockstate(bc, 0) ⊗ spindown(ba)
 ρ0 = dm(fockstate(bc, 0) ⊗ spindown(ba))
 
-# Solve stochastic master equation
-dt = 1e-3
-@time tout, ρt = stochastic.master(T, ρ0, H, J, C; rates=rates, dt=dt);
-
-# %%
-
-C_zeno = [sqrt(κ)*a]
-gₛ_zeno = 1e-3κ
-Hp_zeno = gₛ_zeno*(σ⁺ + σ)
-H_zeno = H0 + Hf + Hp_zeno
-T_zeno = [0:1:3/gₛ_zeno;]
-
-@time tout_zeno, ρt_zeno = stochastic.master(T_zeno, ρ0, H_zeno, J, C_zeno; rates=rates, dt=dt);
-
-# %%
 # Compute time evolution with no coupling to the cavity
-@time tout2, ρt_det = timeevolution.master(T, ρ0, H - g*ad*a*n, J; rates=rates);
+free = timeevolution.master([0:0.1:π;], ρ0, Hp(1)+Hf(), J; rates=rates)
+
+
+function zeno(;dt=1e-3, gₛ = 5*1e-2κ, T = [0:0.1:π/gₛ;])
+	H = H0(g) + Hf(β) + Hp(gₛ)
+	return stochastic.master(T, ρ0, H, J, C; rates=rates, dt=dt)
+end
+
+gₛ=[1e-1, 5e-2, 2.5e-2, 1e-2]
+@time evolution = [zeno(gₛ=i) for i in gₛ];
+
+
+proj = one(bc) ⊗ dm(spindown(ba)) # Calculate expectation value of atoms in |f>
+p0(rho) = real(expect(proj, rho))
 
 #%%
-# Calculate expectation value of atoms in |f>
-proj = one(bc) ⊗ dm(spindown(ba))
-p0 = real(expect(proj, ρt))
-p0_zeno = real(expect(proj, ρt_zeno))
-p0_det = real(expect(proj, ρt_det))
+function plot_evolution(gs, ev)
+	lines!(
+		ev[1].*gs, p0(ev[2]),
+		color=gs, colorrange=(0, maximum(gₛ)),
+		label="$(gs)"
+	)
+end
 
-# %%
 f = Figure(size=fullsize)
-a = Axis(f[1,1], ylabel="<0|ρ|0>", xlabel="gₛt")
-lines!(tout.*gₛ, p0, label="low zeno")
-lines!(tout_zeno.*gₛ_zeno, p0_zeno, label="High zeno")
-lines!(tout2.*gₛ, p0_det, label="Free")
+ax = Axis(f[1,1], ylabel="<0|ρ|0>", xlabel="gₛt")
+lines!(
+	free[1], p0(free[2]),
+	color=:black,
+	label="1"
+)
+for (i, ev) in zip(gₛ, evolution)
+	plot_evolution(i, ev)
+end
+axislegend(ax, position=:rb, framevisible=false, padding=0)
+# Colorbar(f[2,1], colorrange=(0, maximum(gₛ)), vertical=false, label="gs", flipaxis=false)
 f
